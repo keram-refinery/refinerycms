9bbf6274ea3c9407b5fb656bb14d53c4934468a4
Marek Labos <keraml@gmail.com>
2013-07-05 18:31:34 +0200

    update templates

--- templates/refinery/edge.rb
+++ templates/refinery/edge.rb
@@ -1,32 +1,18 @@
 require 'rbconfig'
 
-gsub_file 'Gemfile', "gem 'jquery-rails'", "gem 'jquery-rails', '~> 2.0.0'"
-
-if File.read("#{destination_root}/Gemfile") !~ /assets.+coffee-rails/m
-  gem "coffee-rails", :group => :assets
-end
-
-# We want to ensure that you have an ExecJS runtime available!
-begin
-  run 'bundle install'
-  require 'execjs'
-  ::ExecJS::Runtimes.autodetect
-rescue
-  gsub_file 'Gemfile', "# gem 'therubyracer'", "gem 'therubyracer'"
-end
-
 append_file 'Gemfile' do
 "
 
 gem 'refinerycms', :git => 'git://github.com/refinery/refinerycms.git'
+gem 'refinerycms-i18n', :git => 'git://github.com/refinery/refinerycms-i18n.git'
 
 # USER DEFINED
 
 # Specify additional Refinery CMS Engines here (all optional):
-gem 'refinerycms-i18n', :git => 'git://github.com/refinery/refinerycms-i18n.git'
-gem 'refinerycms-acts-as-indexed', :git => 'git://github.com/refinery/refinerycms-acts-as-indexed.git'
+#  gem 'refinerycms-acts-as-indexed', :git => 'git://github.com/refinery/refinerycms-acts-as-indexed.git'
 #  gem 'refinerycms-blog', :git => 'git://github.com/refinery/refinerycms-blog.git'
 #  gem 'refinerycms-inquiries', :git => 'git://github.com/refinery/refinerycms-inquiries.git'
+#  gem 'refinerycms-calendar', :git => 'git://github.com/refinery/refinerycms-calendar.git'
 #  gem 'refinerycms-search', :git => 'git://github.com/refinery/refinerycms-search.git'
 #  gem 'refinerycms-page-images', :git => 'git://github.com/refinery/refinerycms-page-images.git'
 
--- templates/refinery/installer.rb
+++ templates/refinery/installer.rb
@@ -1,38 +1,21 @@
 require 'rbconfig'
-VERSION_BAND = '2.0.0'
+ROOT_PATH = File.expand_path('../../../', __FILE__)
+require "#{ROOT_PATH}/core/lib/refinery/version.rb"
 
-gsub_file 'Gemfile', "gem 'jquery-rails'", "gem 'jquery-rails', '~> 2.0.0'"
-# We want to ensure that you have an ExecJS runtime available!
-begin
-  run 'bundle install'
-  require 'execjs'
-  raise if ::ExecJS::Runtimes.autodetect.name =~ /therubyracer/
-rescue
-  require 'pathname'
-  if Pathname.new(destination_root.to_s).join('Gemfile').read =~ /therubyracer/
-    gsub_file 'Gemfile', "# gem 'therubyracer'", "gem 'therubyracer'"
-  else
-    append_file 'Gemfile', <<-GEMFILE
-
-group :assets do
-  # Added by Refinery. We want to ensure that you have an ExecJS runtime available!
-  gem 'therubyracer'
-end
-GEMFILE
-  end
-end
+VERSION_BAND = Refinery::Version.to_s
 
 append_file 'Gemfile', <<-GEMFILE
 
 # Refinery CMS
-gem 'refinerycms', '~> #{VERSION_BAND}'
+gem 'refinerycms', '~> #{VERSION_BAND}' # :git => 'git://github.com/refinery/refinerycms.git'
+gem 'refinerycms-i18n', '~> #{VERSION_BAND}' # :git => 'git://github.com/refinery/refinerycms-i18n.git'
 
 # Specify additional Refinery CMS Extensions here (all optional):
-gem 'refinerycms-i18n', '~> #{VERSION_BAND}'
-#  gem 'refinerycms-blog', '~> #{VERSION_BAND}'
-#  gem 'refinerycms-inquiries', '~> #{VERSION_BAND}'
-#  gem 'refinerycms-search', '~> #{VERSION_BAND}'
-#  gem 'refinerycms-page-images', '~> #{VERSION_BAND}'
+#  gem 'refinerycms-blog', '~> #{VERSION_BAND}' # :git => 'git://github.com/refinery/refinerycms-blog.git'
+#  gem 'refinerycms-inquiries', '~> #{VERSION_BAND}' # :git => 'git://github.com/refinery/refinerycms-inquiries.git'
+#  gem 'refinerycms-calendar', '~> #{VERSION_BAND}' # :git => 'git://github.com/refinery/refinerycms-calendar.git'
+#  gem 'refinerycms-search', '~> #{VERSION_BAND}' # :git => 'git://github.com/refinery/refinerycms-search.git'
+#  gem 'refinerycms-page-images', '~> #{VERSION_BAND}' # :git => 'git://github.com/refinery/refinerycms-page-images.git'
 GEMFILE
 
 run 'bundle install'
