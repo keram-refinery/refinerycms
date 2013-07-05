ffd99df2eb66d3e3987f81b5932176ba738ecd53
Marek Labos <keraml@gmail.com>
2013-07-05 22:23:05 +0200

    update dashboard

--- dashboard/lib/refinery/dashboard/engine.rb
+++ dashboard/lib/refinery/dashboard/engine.rb
@@ -1,21 +1,21 @@
 module Refinery
   module Dashboard
     class Engine < ::Rails::Engine
-      extend Refinery::Engine
+      include Refinery::Engine
 
       isolate_namespace Refinery
       engine_name :refinery_dashboard
 
       config.autoload_paths += %W( #{config.root}/lib )
 
-      initializer "register refinery_dashboard plugin" do
+      initializer 'register refinery_dashboard plugin' do
         Refinery::Plugin.register do |plugin|
           plugin.pathname = root
           plugin.name = 'refinery_dashboard'
-          plugin.menu_match = %r{refinery/(refinery_)?dashboard$}
           plugin.always_allow_access = true
           plugin.dashboard = true
-          plugin.url = proc { Refinery::Core::Engine.routes.url_helpers.admin_dashboard_path }
+          plugin.position = 1
+          plugin.url = proc { Refinery::Core::Engine.routes.url_helpers.admin_root_path }
         end
       end
 
