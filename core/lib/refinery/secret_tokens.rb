1efbc0f8d0c4a0d0a7f137a3c1df4236add62c0a
Marek Labos <keraml@gmail.com>
2013-08-27 01:46:01 +0200

    fix: find or secret from ENV + improve readability

--- core/lib/refinery/secret_tokens.rb
+++ core/lib/refinery/secret_tokens.rb
@@ -2,20 +2,20 @@ require 'securerandom'
 
 module Refinery
 
-  def self.find_or_set_secret_token(suffix = nil)
-    token_name = '.secret'
-    token_name << "-#{suffix}" if suffix
+  def self.find_or_set_secret_token(token_name = nil)
+    return ENV[token_name.upcase] if !token_name.nil? && ENV[token_name.upcase].present?
 
-    return ENV[token_name.upcase] if ENV[token_name.upcase].present?
+    token_file = '.secret'
+    token_file << "-#{token_name.downcase}" if token_name
+    token_file_path = Rails.root.join('config', token_file)
 
-    token_file = Rails.root.join('config', token_name)
-    if File.exist? token_file
+    if File.exist? token_file_path
       # Use the existing token.
-      File.read(token_file).chomp
+      File.read(token_file_path).chomp
     else
       # Generate a new token of 64 random hexadecimal characters and store it in token_file.
       token = SecureRandom.hex(64)
-      File.write(token_file, token)
+      File.write(token_file_path, token)
       token
     end
   end
