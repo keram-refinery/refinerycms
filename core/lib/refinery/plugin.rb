b929a0d0ce7b24c1c7839869fafb0da825a0c46b
Marek Labos <keraml@gmail.com>
2013-08-28 00:51:19 +0200

    fix: rake db:reset, when page no exist

--- core/lib/refinery/plugin.rb
+++ core/lib/refinery/plugin.rb
@@ -77,9 +77,7 @@ module Refinery
       if method_name.match(PAGE_METHOD_RE)
         if Refinery::Page.table_exists?
           self.class.send(:define_method, method_name) do
-            instance_variable_get("@#{method_name}") ||
-            instance_variable_set("@#{method_name}",
-              Refinery::Page.find_by(plugin_page_id: "#{self.name}#{'_' << $2 if $2}"))
+            Refinery::Page.find_by(plugin_page_id: "#{self.name}#{'_' << $2 if $2}")
           end
 
           return send method_name
