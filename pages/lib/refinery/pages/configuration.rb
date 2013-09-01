4c450bfec8a3f8defd2c3a9ac02967ac09b8cb9e
Marek Labos <keraml@gmail.com>
2013-09-02 00:37:38 +0200

    removed config main_part and rather document that :body part is necessary

--- pages/lib/refinery/pages/configuration.rb
+++ pages/lib/refinery/pages/configuration.rb
@@ -3,7 +3,7 @@ module Refinery
     include ActiveSupport::Configurable
 
     config_accessor :per_dialog_page, :per_admin_page,
-                    :marketable_urls, :default_parts, :parts, :main_part,
+                    :marketable_urls, :default_parts, :parts,
                     :use_custom_slugs, :scope_slug_by_parent,
                     :layout_template_whitelist,
                     :use_layout_templates,
@@ -15,7 +15,6 @@ module Refinery
     self.marketable_urls = true
     self.parts = [:title, :perex, :body, :side_body]
     self.default_parts = [:title, :body, :side_body]
-    self.main_part = :body
     self.use_custom_slugs = false
     self.scope_slug_by_parent = true
     self.layout_template_whitelist = ['application']
