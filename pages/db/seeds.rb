dc651dc74e52438fb70c1a6d4df35f08beb1c455
Marek Labos <keraml@gmail.com>
2013-09-10 20:01:23 +0200

    seed pages with status "live"

--- pages/db/seeds.rb
+++ pages/db/seeds.rb
@@ -4,6 +4,7 @@ pages = {
   home: {
     title: 'Home',
     deletable: false,
+    status: 'live',
     link_url: '/',
     plugin_page_id: plugin.name
   },
@@ -11,18 +12,21 @@ pages = {
     title: 'Page not found',
     parent: :home,
     deletable: true,
+    status: 'live',
     show_in_menu: false,
     plugin_page_id: "#{plugin.name}_not_found"
   },
   about: {
     title: 'About Us',
     deletable: true,
+    status: 'live',
     show_in_menu: true,
     plugin_page_id: "#{plugin.name}_about"
   },
   colophon: {
     title: 'Colophon',
     deletable: true,
+    status: 'live',
     show_in_menu: false,
     plugin_page_id: "#{plugin.name}_colophon"
   }
