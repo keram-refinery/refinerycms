13d437ba2c0e8fd9d9e756e128ac114d77d058d2
Marek Labos <keraml@gmail.com>
2013-11-21 00:16:35 +0100

    refactorized refinery:form generator
    cleaned code
    better i18n settings handling
    make spam feature configurable and functional

--- core/lib/generators/refinery/form/form_generator.rb
+++ core/lib/generators/refinery/form/form_generator.rb
@@ -7,6 +7,16 @@ module Refinery
 
     include Refinery::ExtensionGeneration
 
+    class_option :spam,
+                 desc: 'Generate extension with spam field',
+                 type: :boolean,
+                 default: false,
+                 required: false
+
+    def includes_spam?
+      options[:spam]
+    end
+
     def description
       "Generates an extension which is set up for frontend form submissions like a contact page."
     end
@@ -21,5 +31,9 @@ module Refinery
       'rails generate refinery:form'
     end
 
+    def reject_file_with_exclude_spam?(file)
+      (!includes_spam? && file.to_s.include?('spam')) || reject_file_without_exclude_spam?(file)
+    end
+    alias_method_chain :reject_file?, :exclude_spam
   end
 end
