234db3d5b6f278357c86a6248a64cfd83b3fcd9f
Marek Labos <keraml@gmail.com>
2013-11-21 18:40:39 +0100

    fix DEPRECATION WARNING: TestCase#process now expects the HTTP method as second argument

--- testing/lib/refinery/testing/controller_macros/methods.rb
+++ testing/lib/refinery/testing/controller_macros/methods.rb
@@ -3,29 +3,29 @@ module Refinery
     module ControllerMacros
       module Methods
         def get(action, parameters = nil, session = nil, flash = nil)
-          process_refinery_action(action, parameters, session, flash, "GET")
+          process_refinery_action(action, "GET", parameters, session, flash)
         end
 
         # Executes a request simulating POST HTTP method and set/volley the response
         def post(action, parameters = nil, session = nil, flash = nil)
-          process_refinery_action(action, parameters, session, flash, "POST")
+          process_refinery_action(action, "POST", parameters, session, flash)
         end
 
         # Executes a request simulating PUT HTTP method and set/volley the response
         def put(action, parameters = nil, session = nil, flash = nil)
-          process_refinery_action(action, parameters, session, flash, "PUT")
+          process_refinery_action(action, "PUT", parameters, session, flash)
         end
 
         # Executes a request simulating DELETE HTTP method and set/volley the response
         def delete(action, parameters = nil, session = nil, flash = nil)
-          process_refinery_action(action, parameters, session, flash, "DELETE")
+          process_refinery_action(action, "DELETE", parameters, session, flash)
         end
 
         private
 
-        def process_refinery_action(action, parameters = nil, session = nil, flash = nil, method = "GET")
+        def process_refinery_action(action, method = "GET", parameters = nil, session = nil, flash = nil)
           parameters ||= {}
-          process(action, parameters.merge!(:use_route => :refinery), session, flash, method)
+          process(action, method, parameters.merge!(:use_route => :refinery), session, flash)
         end
       end
     end
