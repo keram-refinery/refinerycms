0c78919e388338eeeb24aadada7a4632eb451677
Marek Labos <keraml@gmail.com>
2014-01-09 20:09:31 +0100

    removed unused and obsolete ImageHelper content_fu method

--- core/app/helpers/refinery/image_helper.rb
+++ core/app/helpers/refinery/image_helper.rb
@@ -1,21 +1,6 @@
 module Refinery
   module ImageHelper
 
-    # replace all system images with a thumbnail version of them (handy for all images inside a page part)
-    # for example, <%= content_fu(@page.content_for(:body), '96x96#c') %> converts all /system/images to a 96x96 cropped thumbnail
-    def content_fu(content, thumbnail)
-      content.gsub(%r{<img.+?src=['"](/system/images/.+?)/.+?/>}) do |img|
-        begin
-          sha = img.match(%r{/system/images/(.+?)/})[1]
-          job = Dragonfly::Job.deserialize sha, Dragonfly[:refinery_images]
-
-          image_fu Image.where(image_uid: job.uid).first, thumbnail
-        rescue Dragonfly::Serializer::BadString
-          img
-        end
-      end
-    end
-
     # image_fu is a helper for inserting an image that has been uploaded into a template.
     # Say for example that we had a @model.image (@model having a belongs_to :image relationship)
     # and we wanted to display a thumbnail cropped to 200x200 then we can use image_fu like this:
